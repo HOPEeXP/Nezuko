2fa48f4ce44db1758067fbc55e57aa135f6ca303
HOPE <101519089+HOPEeXP@users.noreply.github.com>
2023-02-07 23:59:10 +0530

    Update support.js

--- Organs/commands/Group/support.js
+++ Organs/commands/Group/support.js
@@ -3,7 +3,7 @@ module.exports = {
 	alias: ["sup"],
 	desc: "Send you official support group link.",
 	category: "Group",
-    react:"✅",
+    react:"🍃",
     start:async(client,m,{command,prefix,pushName})=>{
         let yup = ['https://media.tenor.com/XikoNQDfaqcAAAPo/bunny-nezuko-bunny-ears.mp4',
                    'https://media.tenor.com/xBh07rz9GHYAAAPo/nezuko-kamado-nezuko.mp4',
@@ -20,14 +20,14 @@ const eternitylogogg = "https://i.ibb.co/t3Jd1wK/Eternity-6.png"
 const lemo = `
 *━『 Support Group Links 』━*
 
-*1) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ ] :*
-https://chat.whatsapp.com/JCCZPbPUbM1536n62zSFZi
+*1) [ ▼△▼△ 𝚃𝙾𝙼𝙰𝙽 ▼△▼△ ] :*
+https://chat.whatsapp.com/Hxpr3MtH79CIpGF4vG7NZ7
 
-*2) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ ₵𝖆𝖘𝖎𝖓ø ] 🎰 :*
-https://chat.whatsapp.com/J1WfFPXtmzZGXFB59I0ETq
+*2) [ ⭑⭑⭑★ 𝐁𝐎𝐍𝐓𝐄𝐍 ★⭑⭑⭑ ]  :*
+https://chat.whatsapp.com/CQKbWP7c3GNIk4SfE7Xej7
 
-*3) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ N̸s̸f̸w̸ ] :*
-https://chat.whatsapp.com/J1WfFPXtmzZGXFB59I0ETq
+*3) [ ▍║▍▏║ VALHALLA ║▍▏║▍ ] :*
+https://chat.whatsapp.com/CP34X93ReEY2vdp8AakAbe
 `
   await client.sendMessage(m.from,{video:{url:rae}, gifPlayback:true, caption: `*${pushName}* have a look in your DM`},{quoted:m})
   await  client.sendMessage(m.sender,{image:{url:eternitylogogg},caption:lemo},{quoted:m})  
